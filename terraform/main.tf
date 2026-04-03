77ea004ef34da6d32eedda59cd1bfdd84c1f4f42
Elakiya-30 <elakiyachezhiyan30@gmail.com>
2026-04-03 13:08:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "game_server" {
   ami           = "ami-05d2d839d4f73aafb" 
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   key_name      = "linuxnew"
 
   security_groups = [aws_security_group.NewGame.name]
